97aac5d0a4d7220f17012cbd952502c0c6f1c61c
Boluwatife <boluwatifeoladejo@gmail.com>
2022-12-19 17:00:55 +0100

    Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myVar = () => {
+  const myList = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+  for (let i = 0; i < myList.length; i++) {
+    console.log(myList[i]);
+  }
+};
+myVar();
